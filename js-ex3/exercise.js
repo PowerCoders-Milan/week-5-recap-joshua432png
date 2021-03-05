e18d597f50ac397367a0e7b5d0295f68272a7736
Joshua Udeh <udehasilo@gmail.com>
2021-03-05 17:07:08 +0100

    js3

--- js-ex3/exercise.js
+++ js-ex3/exercise.js
@@ -1,4 +1,4 @@
-formula(1, 3, 5) //=> 8
+/*formula(1, 3, 5) //=> 8
 formula(3, 4, 6) //=> 18
 formula(2, 4) //=> 9
 formula(5, 6) //=> 31
@@ -9,4 +9,18 @@ formula() //=> 2
 // review all the possible parameters
 function formula(){
 
+}*/
+
+console.log(formula(1, 3, 5)) //=> 8
+console.log(formula(3, 4, 6)) //=> 18
+console.log(formula(2, 4)) //=> 9
+console.log(formula(5, 6)) //=> 31
+console.log(formula(3)) //=> 4
+console.log(formula(5)) //=> 6
+console.log(formula()) //=> 2
+
+// review all the possible parameters
+function formula(num1 = 1, num2 = 1, num3 = 1){
+    return num2 * num1 + num3;
 }
+
